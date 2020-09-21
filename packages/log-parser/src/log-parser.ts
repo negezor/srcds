b5442f22650d93c67f697a014c3294a452946995
Negezor <negezor@gmail.com>
2020-09-22 04:14:13 +1100

    refactor(log-parser): use strong import for parser

--- packages/log-parser/src/log-parser.ts
+++ packages/log-parser/src/log-parser.ts
@@ -1,16 +1,95 @@
-import * as rawParsers from './parsers';
+import {
+	assistParser,
+	attackedParser,
+	connectionParser,
+	entityTriggeredParser,
+	killedParser,
+	playerUpdateParser,
+	projectileSpawnedParser,
+	purchasedParser,
+	rconParser,
+	sayParser,
+	scoredParser,
+	serverCVarParser,
+	ServerLogParser,
+	suicideParser,
+	switchedTeamParser,
+	teamNameParser,
+	teamTriggeredParser,
+	threwParser,
+	validatedParser,
+	warmodParser,
+
+	AssistEvent,
+	AttackedEvent,
+	ConnectionEvent,
+	EntityTriggeredEvent,
+	KilledEvent,
+	PlayerUpdateEvent,
+	ProjectileSpawnedEvent,
+	PurchasedEvent,
+	RconEvent,
+	SayEvent,
+	ScoredEvent,
+	ServerCVarEvent,
+	ServerLogEvent,
+	SuicideEvent,
+	SwitchedTeamEvent,
+	TeamNameEvent,
+	TeamTriggeredEvent,
+	ThrewEvent,
+	ValidatedEvent,
+	WarmodEvent
+} from './parsers';
+
+export const defaultParsers = [
+	assistParser,
+	attackedParser,
+	connectionParser,
+	entityTriggeredParser,
+	killedParser,
+	playerUpdateParser,
+	projectileSpawnedParser,
+	purchasedParser,
+	rconParser,
+	sayParser,
+	scoredParser,
+	serverCVarParser,
+	ServerLogParser,
+	suicideParser,
+	switchedTeamParser,
+	teamNameParser,
+	teamTriggeredParser,
+	threwParser,
+	validatedParser,
+	warmodParser
+];
+
+export type Events =
+| AssistEvent
+| AttackedEvent
+| ConnectionEvent
+| EntityTriggeredEvent
+| KilledEvent
+| PlayerUpdateEvent
+| ProjectileSpawnedEvent
+| PurchasedEvent
+| RconEvent
+| SayEvent
+| ScoredEvent
+| ServerCVarEvent
+| ServerLogEvent
+| SuicideEvent
+| SwitchedTeamEvent
+| TeamNameEvent
+| TeamTriggeredEvent
+| ThrewEvent
+| ValidatedEvent
+| WarmodEvent;
 
 const { length: LENGTH_OF_DATE } = '10/20/2020 - 10:30:50: ';
 
-const parsers = Object.values(rawParsers)
-	.filter(item => (
-		typeof item === 'object'
-		&& Reflect.has(item, 'type')
-		&& Reflect.has(item, 'patterns')
-		&& Reflect.has(item, 'parse')
-	));
-
-export function parse(rawLog: string) {
+export function parse(rawLog: string): Events | undefined {
 	const receivedAt = new Date(
 		rawLog
 			.substring(0, LENGTH_OF_DATE - 2)
@@ -19,7 +98,7 @@ export function parse(rawLog: string) {
 
 	const log = rawLog.substring(LENGTH_OF_DATE);
 
-	for (const parser of parsers) {
+	for (const parser of defaultParsers) {
 		const pattern = parser.patterns.find(item => (
 			item.test(log)
 		));
@@ -38,7 +117,7 @@ export function parse(rawLog: string) {
 			receivedAt,
 
 			payload
-		};
+		} as Events;
 	}
 
 	return undefined;
